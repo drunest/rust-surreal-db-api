dc5d6cf2999b9872025cfeaef0b71a68f29ea9b6
Radha743 <worldofnara777@gmail.com>
2023-11-09 15:47:59 +0530

    Signin Cleanup

--- src/api/routes/signin.rs
+++ src/api/routes/signin.rs
@@ -14,16 +14,42 @@ pub struct SignInBody {
     pub email_id: Option<String>,
     pub password: String,
 }
+/// Checks and return if the user is using username or email to login  
 fn get_auth_cred_method(
     username: Option<String>,
     email_id: Option<String>,
-) -> (UserFindableCol, String) {
-    match (username, email_id) {
+) -> Result<(UserFindableCol, String), AppError> {
+    if username.is_none() && email_id.is_none() {
+        return Err(AppError::BadRequest(
+            "You need to provide either the 'username' for 'email_id'".into(),
+        ));
+    }
+
+    let method = match (username, email_id) {
         (Some(username), _) => (UserFindableCol::Username, username),
         (_, Some(email)) => (UserFindableCol::EmailId, email),
         _ => unreachable!(), // You've already checked for both being None above
+    };
+    Ok(method)
+}
+
+fn match_user_exists(
+    user: Option<User>,
+    auth_method: UserFindableCol,
+    value: String,
+) -> Result<User, AppError> {
+    match user {
+        Some(user) => Ok(user),
+        None => {
+            let method: String = auth_method.clone().into();
+            return Err(AppError::BadRequest(format!(
+                "User with {} {} not found...",
+                method, value
+            )));
+        }
     }
 }
+
 pub async fn login(
     db: web::Data<Surreal<Client>>,
     body: web::Json<SignInBody>,
@@ -33,45 +59,33 @@ pub async fn login(
     let email_id = body.email_id.to_owned();
     let password = body.password.to_owned();
 
-    if username.is_none() && email_id.is_none() {
-        return Err(AppError::BadRequest(
-            "You need to provide either the 'username' for 'email_id'".into(),
-        ));
-    }
+    // user can either log in with the username or email_id
+    let (auth_method, value) = get_auth_cred_method(username, email_id)?;
 
-    let (auth_method, val) = get_auth_cred_method(username, email_id);
+    // Find the user from the database
+    let user = User::find_one(&db, auth_method.clone(), value.clone()).await?;
 
-    let user = User::find_one(&db, auth_method.clone(), val.clone()).await?;
+    // Match If the user exists in the database
+    let user = match_user_exists(user, auth_method, value)?;
 
-    match user {
-        Some(user) => {
-            let check_password = user.verify_password(&password).await?;
+    let check_password = user.verify_password(&password).await?;
 
-            match check_password {
-                true => {
-                    let slim_user = SlimUser::from(&user);
-                    Identity::login(&req.extensions(), slim_user.id.clone()).map_err(|err| {
-                        dbg!(err);
-                        AppError::InternalError("Something Went Wrong".into())
-                    })?;
+    match check_password {
+        true => {
+            let slim_user = SlimUser::from(&user);
+            Identity::login(&req.extensions(), slim_user.id.clone()).map_err(|err| {
+                dbg!(err);
+                AppError::InternalError("Something Went Wrong".into())
+            })?;
 
-                    Ok(HttpResponse::Ok().json(json!(
-                    {"status": "success",
-                    "msg": "logging_in",
-                    "user": slim_user,
-                    })))
-                }
-                false => {
-                    return Err(AppError::BadRequest("Provided Incorrect Password..".into()));
-                }
-            }
+            Ok(HttpResponse::Ok().json(json!(
+            {"status": "success",
+            "msg": "logging_in",
+            "user": slim_user,
+            })))
         }
-        None => {
-            let method: String = auth_method.clone().into();
-            return Err(AppError::BadRequest(format!(
-                "User with {} {} not found...",
-                method, &val
-            )));
+        false => {
+            return Err(AppError::BadRequest("Provided Incorrect Password..".into()));
         }
     }
 }
